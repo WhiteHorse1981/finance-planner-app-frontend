75adb23421cbebf8ab588cf5a7046918f6727223
Andrey Belokon <belokon1981andrey@gmail.com>
2023-04-30 22:26:01 +0300

    1

--- src/components/PublicRoute/PublicRoute.jsx
+++ src/components/PublicRoute/PublicRoute.jsx
@@ -1,14 +1,9 @@
 import { useSelector } from 'react-redux';
 import { Navigate, Outlet } from 'react-router-dom';
-import { selectToken, balance } from 'redux/auth/auth-selectors';
+import { selectToken } from 'redux/auth/auth-selectors';
 
 export default function PublicRoute() {
   const token = useSelector(selectToken);
-  const balanceUser = useSelector(balance);
 
-  return token && balanceUser !== 0 ? (
-    <Navigate to="/personal-plan" />
-  ) : (
-    <Outlet />
-  );
+  return token ? <Navigate to="/personal-plan" /> : <Outlet />;
 }
